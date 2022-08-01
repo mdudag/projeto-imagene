182c994e92663a11c4934980e1c6a3984544bb06
Duda <106630667+mdudag@users.noreply.github.com>
2022-07-31 23:11:02 -0300

    Add files via upload
     Apresentação dos resultados da pesquisa, adição do link pelos elementos do cabeçalho para a tela inicial e responsividade

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let primeiraPesq = true
+
 document.addEventListener("keypress", function(e) {
     if (e.key === "Enter") {
         document.querySelector('.quadroPesq2').style.display = "block"
@@ -5,21 +7,34 @@ document.addEventListener("keypress", function(e) {
         document.querySelector('#quadroDestaques').style.display = "none"
         document.querySelector('#quadroImg').style.display = "none"
 
-        function fazGet(url) {
+        function fazGet(url) {    // função que conecta com a API e retorna os dados referentes ao que o usuário digitou
             let request = new XMLHttpRequest()
             request.open("GET", url, false)
             request.send()
             return request.responseText
         }
 
-        function main() {
+        function main() {   // configura a conexão com a API e implementa as imagens da pesquisa no HTML
+            if (primeiraPesq) pesq = document.querySelector('#pesq1').value
+            else {
+                resultPesq = document.querySelector('#resultPesq')
+                resultPesq.innerHTML = ""
+                pesq = document.querySelector('.pesq2').value
+            }
+
+            primeiraPesq = false
             let API_KEY = '28956871-8ea1618e9953c308ba075e288'
-            let URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses')
+            let URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(pesq)
             let data = fazGet(URL)
             let result = JSON.parse(data)
             
             result.hits.forEach(element => {
-                urlImg = element.webformatURL
+                img = document.createElement('img')
+                img.src = element.webformatURL
+                img.style.width = "400px"
+                img.style.marginRight ="5px" 
+
+                document.querySelector('#resultPesq').appendChild(img)
             });
         }
 
